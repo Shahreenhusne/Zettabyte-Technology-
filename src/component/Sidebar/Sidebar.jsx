82fca754fa4df7feb06354824348719bd899116c
Shahreen <shahreen.husne.rabbani@gmail.com>
2025-09-21 05:40:50 +0600

    feat: add header and sidebar components

---
+++ src/component/Sidebar/Sidebar.jsx
@@ -0,0 +1,70 @@
+'use client'
+import React, { useState } from 'react'
+import {  House, Menu } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link';
+
+
+
+const menuItems = [
+  {
+    title: "MENU",
+    items: [
+      {
+        icon: House,
+        label: "Home",
+        href: "/",
+      }
+      
+       
+     
+    ],
+  },
+ 
+];
+
+
+const Sidebar = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const pathname = usePathname();
+  return (
+      <div className={`relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 ${isSidebarOpen ? "w-64" : "w-20"}`}>
+        <div className='h-full bg-[#1e1e1e] backdrop-blur-md p-4 flex flex-col border-r border-[#2f2f2f]'>
+            <button onClick={() => setIsSidebarOpen(!isSidebarOpen)} className='p-2 rounded-full hover:bg-[#2f2f2f] transition-all cursor-pointer'>
+                <Menu size={24}/>
+            </button>
+            {isSidebarOpen && 
+            (
+               <nav className='mt-8 flex-grow'>
+                          {menuItems.map((section) =>
+                            section.items.map((item) => {
+                              const IconComponent = item.icon;
+                              return (
+                                <Link key={item.label} href={item.href}>
+                                  <div
+                                    className={`flex items-center p-4 text-sm font-medium rounded-lg hover:bg-[#2f2f2f] transition-colors mb-2 ${
+                                      pathname === item.href ? "bg-[#2f2f2f]" : ""
+                                    }`}
+                                  >
+                                    <IconComponent size={20} style={{ minWidth: "20px" }} />
+                                     <span className='ml-4 whitespace-nowrap'>{item.label}</span>
+                                   
+                                  </div>
+                                </Link>
+                              );
+                            })
+                          )}
+                       </nav>
+                                     
+        )}      
+          </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
+
+
+ 
+
